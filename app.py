4e1a37dbf9f69da2fe65d3939d65655ec27dadec
JabezSanjay <43309046+JabezSanjay@users.noreply.github.com>
2020-01-04 11:47:53 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,30 @@
+import tkinter as tk
+
+
+root = tk.Tk()
+# changed by Jabez
+# Adding widgets
+root.option_add("*Button.Background", "black")
+root.option_add("*Button.Foreground", "red")
+
+
+root.title('The game')
+# Set the geometry attribute to change the root windows size
+root.geometry("1920x1200")  # Size of the window is 1920 x 1200
+root.resizable(0, 0)  # Don't allow resizing in the x or y direction
+
+back = tk.Frame(root, bg='black')
+
+# Don't allow the widgets inside to determine the frame's width / height
+back.pack_propagate(0)
+# Expand the frame to fill the root window
+back.pack(fill=tk.BOTH, expand=1)
+
+# Adding variables and pack()
+
+go = tk.Button(master=back, text='Start Game')
+go.pack()
+close = tk.Button(master=back, text='Quit', command=root.destroy)
+close.pack()
+
+root.mainloop()
